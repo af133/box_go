4adfb00665356b30f7ddb626b203dea10c2d1847
Andre Firmansyah <af349393@gmail.com>
2025-11-20 16:56:30 +0700

    update

--- app/Http/Controllers/MitraBarang.php
+++ app/Http/Controllers/MitraBarang.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\CssSelector\XPath\Extension\FunctionExtension;
-
+use App\Models\Lokasi;
 class MitraBarang extends Controller
 {
     public function ShowBarangMitra(){
-        
+        $lokasi=Lokasi::with(['area_gudang.polygon','mitra.harga_mitra.jenis_barang'])->get();
+        return response()->json([
+            'lokasi'=>$lokasi
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MitraBarang;
 use App\Http\Controllers\Profile;
 use Illuminate\Support\Facades\Route;
 Route::get('/hello', function() {
     return response()->json(['message' => 'Hello, World!']);
 });
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/profile/update',[Profile::class,'update']);
+Route::get('/mitra/lokasi',[MitraBarang::class,'ShowBarangMitra']);
